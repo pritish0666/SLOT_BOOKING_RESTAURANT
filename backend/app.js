ee4bb036683d90bce1d1166417ea6ceae05f6910
Pritish <pritishpatra06@gmail.com>
2024-09-03 22:28:21 +0530

    bug fixex & deployment

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,11 @@ dotenv.config({ path: "./config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [
+      process.env.FRONTEND_URL,
+      "https://66d70eb4064dad3ac5d73393--nimble-paprenjak-f6e224.netlify.app/",
+      "https://66d73ff06f1557822fd7e4a0--snazzy-cannoli-2a7863.netlify.app/",
+    ],
     methods: ["POST"],
     credentials: true,
   })
@@ -19,10 +23,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/reservation", reservationRouter);
-app.get("/", (req, res, next)=>{return res.status(200).json({
-  success: true,
-  message: "HELLO WORLD AGAIN"
-})})
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    success: true,
+    message: "HELLO WORLD AGAIN",
+  });
+});
 
 dbConnection();
 
