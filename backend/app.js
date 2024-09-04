55c35ba8c7de82f939f5fa36770d0436eb0454ac
Pritish <pritishpatra06@gmail.com>
2024-09-04 07:42:52 +0530

    cors update

--- backend/app.js
+++ backend/app.js
@@ -8,17 +8,7 @@ import { dbConnection } from "./database/dbConnection.js";
 const app = express();
 dotenv.config({ path: "./config.env" });
 
-app.use(
-  cors({
-    origin: [
-      process.env.FRONTEND_URL,
-      "https://66d70eb4064dad3ac5d73393--nimble-paprenjak-f6e224.netlify.app/",
-      "https://66d73ff06f1557822fd7e4a0--snazzy-cannoli-2a7863.netlify.app/",
-    ],
-    methods: ["POST"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
